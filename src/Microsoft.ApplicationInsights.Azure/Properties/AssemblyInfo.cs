329b683c06691848ce823a5e85a9be00f572c3e6
Eric Maino <ericmai@microsoft.com>
2015-12-09 09:52:12 -0800

    Updating assembly version to 1.2

--- src/Microsoft.ApplicationInsights.Azure/Properties/AssemblyInfo.cs
+++ src/Microsoft.ApplicationInsights.Azure/Properties/AssemblyInfo.cs
@@ -5,19 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCopyright("Copyright © Microsoft. All Rights Reserved.")]
 [assembly: AssemblyTitle("Microsoft.ApplicationInsights.Azure")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Microsoft.ApplicationInsights.Azure")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4addda7f-4742-4ad6-9e8f-265d8d579d2f")]
@@ -32,5 +25,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.*")]
+[assembly: AssemblyVersion("1.2.*")]
+[assembly: AssemblyFileVersion("1.2")]
