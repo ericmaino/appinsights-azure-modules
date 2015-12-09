8319e1f8d3786323dafec28a4131b87d7c09aa1e
Eric Maino <ericmai@microsoft.com>
2015-12-08 17:09:04 -0800

    Initial commit of AzureConfigurationModule and solution

---
+++ src/Microsoft.ApplicationInsights.Azure/Extensibility/AzureConfigurationModule.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Azure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.ApplicationInsights.Extensibility
+{
+    public class AzureConfigurationModule : ITelemetryModule
+    {
+        public AzureConfigurationModule()
+        {
+            ProductionKeyName = "ApplicationInsightsKey";
+        }
+
+        public string DeveloperKey { get; set; }
+
+        public string ProductionKeyName { get; set; }
+
+        public void Initialize(TelemetryConfiguration configuration)
+        {
+            var productionKey = String.Empty;
+
+            if (!String.IsNullOrEmpty(ProductionKeyName))
+            {
+                productionKey = CloudConfigurationManager.GetSetting(ProductionKeyName);
+            }
+
+            if (String.IsNullOrEmpty(productionKey))
+            {
+                configuration.TelemetryChannel.DeveloperMode = true;
+                productionKey = DeveloperKey;
+            }
+
+            configuration.InstrumentationKey = productionKey;
+        }
+    }
+}
